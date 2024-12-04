35639c784d37d2d9973091054ee914080d9a7256
pateltusharv <patel.tusharv@northeastern.edu>
2024-12-03 19:12:21 -0500

    added user APIs

---
+++ api/controllers/userController.js
@@ -0,0 +1,49 @@
+import * as userService from '../services/userService.js';
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await userService.getUserById(req.params.userId);
+    if (!user) return res.status(400).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const user = await userService.updateUser(req.params.userId, req.body);
+    if (!user) return res.status(400).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await userService.deleteUser(req.params.userId);
+    if (!user) return res.status(400).json({ message: 'User not found' });
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
