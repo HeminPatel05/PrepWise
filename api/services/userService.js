11406461a87aaefc3f3606160a7ed87446c5ab07
pateltusharv <patel.tusharv@northeastern.edu>
2024-12-03 19:13:41 -0500

    added user services

---
+++ api/services/userService.js
@@ -0,0 +1,26 @@
+import User from '../models/user.js';
+
+export const getAllUsers = async () => {
+  return await User.find();
+};
+
+export const getUserById = async (id) => {
+  return await User.findById(id);
+};
+
+export const createUser = async (userData) => {
+  const existingUser = await User.findOne({ email: userData.email });
+  if (existingUser) {
+    throw new Error('User already exists');
+  }
+  const user = new User(userData);
+  return await user.save();
+};
+
+export const updateUser = async (id, userData) => {
+  return await User.findByIdAndUpdate(id, userData, { new: true });
+};
+
+export const deleteUser = async (id) => {
+  return await User.findByIdAndDelete(id);
+};
