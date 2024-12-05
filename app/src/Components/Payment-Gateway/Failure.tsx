df3254eb0d867d92a4acedaa9a1b55a4b461e460
Hemin Patel <patelhemin55@gmail.com>
2024-12-05 17:55:56 -0500

    chanegd message in faliure of payment

--- app/src/Components/Payment-Gateway/Failure.tsx
+++ app/src/Components/Payment-Gateway/Failure.tsx
@@ -7,7 +7,6 @@ const Failure = () => {
       <div className="failure-card">
         <h1>Payment Failed 😞</h1>
         <p>Something went wrong while processing your payment.</p>
-        <p>Please try again or contact support if the issue persists.</p>
         <button
           className="failure-button retry"
           onClick={() => (window.location.href = "/premium")}
