c6d3ca3d5af891801ee092b67effa090d32c02e0
pateltusharv <patel.tusharv@northeastern.edu>
2024-12-08 02:32:35 -0500

    Branch clean

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-import progressRouter from "./progressRoutes.js";
-import paymentRouter from "./paymentRoutes.js";
-const initializeRoutes = (app) => {
-  app.use("/progress", progressRouter);
-  app.use("/payment", paymentRouter);
-};
-
-export default initializeRoutes;
