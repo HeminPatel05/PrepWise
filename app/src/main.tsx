a21d337ee0b900b62148b538b2b40f59e17990fe
pateltusharv <patel.tusharv@northeastern.edu>
2024-12-07 19:40:51 -0500

    Routing update

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./services/store";
 import App from "./App";
+import '../src/config/i18n'; // Import i18n configuration
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
